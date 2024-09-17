5b26026e507e0008bf59462bdba5883fdbf47c82
Monique <cabigtingmoniquekyle@gmail.com>
2024-09-17 23:05:55 +0800

    Revert "update"
    This reverts commit 95fa14fb2251cf84c7ffef410598c860a645447f.

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login/LoginForm';
 import AdminHomePage from './admin/AdminHomePage';
