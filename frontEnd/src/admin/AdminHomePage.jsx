a70966d5134bbdabba052b375178d445c09ba7b8
Clarieza Bautista <145357759+imvelvet22@users.noreply.github.com>
2024-10-05 21:18:43 +0800

    frontend
    project side

--- frontEnd/src/admin/AdminHomePage.jsx
+++ frontEnd/src/admin/AdminHomePage.jsx
@@ -8,14 +8,12 @@ function AdminHomePage() {
                 <ul>
                     <li><Link to="/admin">Dashboard</Link></li>
                     <li><Link to="/project">Projects</Link></li>  {/* Links to Project Management */}
-                    <li><Link to="/tasks">Tasks</Link></li>
-                    <li><Link to="/documents">Documents</Link></li>
                     <li><Link to="/reports">Reports</Link></li>
                     <li><Link to="/calendar">Calendar</Link></li>
                     <li><Link to="/create">Create</Link></li>
                     <li><Link to="/clients">Clients</Link></li>
                     <li><Link to="/employees">Employees</Link></li>
-                    <li><Link to="/survey">Survey</Link></li>
+                    <li><Link to="/Logout">Logout</Link></li>
                 </ul>
             </div>
             <div className="content">
