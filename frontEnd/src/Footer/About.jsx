d8a12865c8bcdcbc7b4d5222fab6697573ea6042
Monique <cabigtingmoniquekyle@gmail.com>
2024-10-06 08:11:13 +0800

    oki naa

---
+++ frontEnd/src/Footer/About.jsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello, World! This is the About component.</h1>
+    </div>
+  );
+}
+
+export default About;
--- frontEnd/src/admin/AdminHomePage.jsx
+++ frontEnd/src/admin/AdminHomePage.jsx
@@ -1,40 +1,25 @@
+
 import './AdminHomePage.css';
-import ClientList from './clientlist'; // Import the updated ClientList component
-import { useState } from 'react';
 import { Link } from 'react-router-dom';  // Import Link from React Router
 
 function AdminHomePage() {
-    const [activeTab, setActiveTab] = useState('dashboard');
-
     return (
         <div className="admin-home-page">
             <div className="sidebar">
                 <ul>
-                    <li><Link to="/calendar">Calendar</Link></li>
                     <li><Link to="/admin">Dashboard</Link></li>
                     <li><Link to="/project">Projects</Link></li>  {/* Links to Project Management */}
-                    <li><Link to="/tasks">Tasks</Link></li>
-                    <li><Link to="/documents">Documents</Link></li>
+                    <li><Link to="/reports">Reports</Link></li>
                     <li><Link to="/calendar">Calendar</Link></li>
                     <li><Link to="/create">Create</Link></li>
                     <li><Link to="/clients">Clients</Link></li>
                     <li><Link to="/employees">Employees</Link></li>
-                    <li><Link to="/reports">Reports</Link></li>
-                    
+                    <li><Link to="/Logout">Logout</Link></li>
                 </ul>
             </div>
-
             <div className="content">
-                {activeTab === 'dashboard' && (
-                    <>
-                        <h2>Admin Dashboard</h2>
-                        <h1>Overview</h1>
-                    </>
-                )}
-
-                {activeTab === 'clients' && (
-                    <ClientList />  // Display the ClientList component
-                )}
+                <h2>Admin Dashboard</h2>
+                <h1>Overview</h1>
             </div>
         </div>
     );
